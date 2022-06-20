c2a3b047baf9ee246a87f4bd7c4cab8b82e385e6
Ray0Emma <ahmadifarah22@gmail.com>
2022-06-20 20:03:24 +0000

    feat: added axios and vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+  <div class="container">
+    <InstructorApp />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import InstructorApp from "./components/InstructorApp.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    InstructorApp,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import url(https://unpkg.com/bootstrap@4.1.0/dist/css/bootstrap.min.css);
 </style>
---
+++ src/components/InstructorApp.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>This is instructors test!</div>
+</template>
+
+<script>
+export default {
+  name: "InstructorApp",
+};
+</script>
+
+<style scoped></style>
